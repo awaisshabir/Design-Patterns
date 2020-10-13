95e3d5b717d25fa9c303f8fea5ff95706a2d6ea5
awaisshabir <awais.shabir@cloudsolution.com.sa>
2020-10-13 17:54:25 +0300

    initial facade pattern

---
+++ DesignPatterns/Facade/FacadeImplementation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Facade
+{
+  public  class FacadeImplementation
+    {
+        private IShape BMW;
+        private IShape HoundaBike;
+        public FacadeImplementation()
+        {
+            BMW = new Car();
+            HoundaBike = new Bike();
+        }
+        public void DrawCar()
+        {
+            BMW.Draw();
+        }
+        public void DrawBike()
+        {
+            HoundaBike.Draw();
+        }
+    }
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Facade
+
+            FacadeCalling();
+
+            Console.ReadKey();
+        }
+
+        private static void FacadeCalling()
+        {
+            Facade.FacadeImplementation facade = new Facade.FacadeImplementation();
+            facade.DrawCar();
+            facade.DrawBike();
+        }
+    }
+}
